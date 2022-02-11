90af4b5af5c7cee8e8414ddf192df5e310f85276
p6majo <p6majo@gmx.de>
2022-02-11 21:05:17 +0100

    Vielfache von 5

---
+++ VielfachesBerechnen.java
@@ -0,0 +1,11 @@
+public class VielfachesBerechnen
+{
+    public void VielfacheBerechnen()
+    {   
+        for (int i=0;i<=1000;i++)
+        {
+            int vielfaches = 5*i;
+            System.out.println(vielfaches);
+        }
+    }
+}
